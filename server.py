26a66e7fd9039f15cda060a00cd33fda1f9fbf27
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-10-14 14:26:59 +0200

    Create satellite class that will cache data

--- server.py
+++ server.py
@@ -8,34 +8,54 @@ app = Flask(__name__)
 api = Api(app)
 
 
+class Satellite:
+    """
+    This is a class containing all data about the satellite
+
+    Use instance of this when serving http responses
+    """
+
+    def __init__(self):
+        self.is_connected: bool = False
+        self.battery_percentage: int = None
+        self.cpu_temperature: float = None
+        self.lora_signal_rssi: int = None
+        self.location_lat: float = None
+        self.location_lng: float = None
+        self.location_height: float = None
+
+
+sat = Satellite()
+
+
 @api.route('/satellite')
 class Satellite(Resource):
     def get(self):
-        return {'connected': True}
+        return {'connected': sat.is_connected}
 
 
 @api.route('/satellite/battery')
 class SatelliteBattery(Resource):
     def get(self):
-        return {'percentage': 69}
+        return {'percentage': sat.battery_percentage}
 
 
 @api.route('/satellite/cpu/temperature')
 class SatelliteCpuTemperature(Resource):
     def get(self):
-        return {'celsius': 47}
+        return {'celsius': sat.cpu_temperature}
 
 
 @api.route('/satellite/lora/signal')
 class SatelliteLoraSignal(Resource):
     def get(self):
-        return {'rssi': -81}
+        return {'rssi': sat.lora_signal_rssi}
 
 
 @api.route('/satellite/location')
 class SatelliteLocation(Resource):
     def get(self):
-        return {'latitude': 49.88345, 'longitude': 19.49253, 'height': 2137}
+        return {'latitude': sat.location_lat, 'longitude': sat.location_lng, 'height': sat.location_height}
 
 
 if __name__ == '__main__':
