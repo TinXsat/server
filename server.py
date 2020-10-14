148699cd53f01736469580776b0ef1138702c02c
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-10-14 13:42:00 +0200

    Some example endpoints with placeholders

--- server.py
+++ server.py
@@ -11,5 +11,30 @@ def hello_there():
     return 'Hello from TinX server!'
 
 
+@app.route('/satellite')
+def satellite():
+    return {'connected': True}
+
+
+@app.route('/satellite/battery')
+def satellite_battery():
+    return {'percentage': 69}
+
+
+@app.route('/satellite/cpu/temperature')
+def satellite_cpu_temperature():
+    return {'celsius': 47}
+
+
+@app.route('/satellite/lora/signal')
+def satellite_lora_signal():
+    return {'rssi': -81}
+
+
+@app.route('/satellite/location')
+def satellite_location():
+    return {'latitude': 49.88345, 'longitude': 19.49253, 'height': 2137}
+
+
 if __name__ == '__main__':
     app.run()
